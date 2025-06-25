39122ce67c6ddd37b20b0cb014a65a16fff2a87d
Kylver21 <kilverpaucar1@gmail.com>
2025-06-25 14:46:09 -0500

    Fix component import paths in routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-  { path: 'productos', loadComponent: () => import('./pages/product-list/product-list.component').then(m => m.ProductListComponent) },
-  { path: 'producto/:id', loadComponent: () => import('./pages/product-detail/product-detail.component').then(m => m.ProductDetailComponent) },
-  { path: 'carrito', loadComponent: () => import('./pages/cart/cart.component').then(m => m.CartComponent) },
-  { path: 'checkout', loadComponent: () => import('./pages/checkout/checkout.component').then(m => m.CheckoutComponent) },
-  { path: 'perfil', loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent) },
+  { path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'productos', loadComponent: () => import('./components/product-list/product-list.component').then(m => m.ProductListComponent) },
+  { path: 'producto/:id', loadComponent: () => import('./components/product-detail/product-detail.component').then(m => m.ProductDetailComponent) },
+  { path: 'carrito', loadComponent: () => import('./components/cart/cart.component').then(m => m.CartComponent) },
+  { path: 'checkout', loadComponent: () => import('./components/checkout/checkout.component').then(m => m.CheckoutComponent) },
+  { path: 'perfil', loadComponent: () => import('./components/profile/profile.component').then(m => m.ProfileComponent) },
   { path: '**', redirectTo: '' }
 ];
